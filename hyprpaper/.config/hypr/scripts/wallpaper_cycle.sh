3d3f304bf11d9cddaa509b5b9742c0d3a93920f1
Camille Vuillaume <camillevuillaume@gmail.com>
2025-06-06 22:37:35 +0900

    moved scripts to dedicated folder

---
+++ hyprpaper/.config/hypr/scripts/wallpaper_cycle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+current_hour=$(date +%H)
+
+sleep 2
+
+if [ $current_hour -ge 5 -a $current_hour -le 7 ]; then
+    hyprctl --instance 0 hyprpaper wallpaper ,"~/.config/backgrounds/tokyo_dusk.jpg"
+elif [ $current_hour -ge 8 -a $current_hour -le 18 ]; then
+    hyprctl --instance 0 hyprpaper wallpaper ,"~/.config/backgrounds/tokyo_day.jpg"
+elif [ $current_hour -ge 19 -a  $current_hour -le 21 ]; then
+    hyprctl --instance 0 hyprpaper wallpaper ,"~/.config/backgrounds/tokyo_dusk.jpg"
+else
+    hyprctl --instance 0 hyprpaper wallpaper ,"~/.config/backgrounds/tokyo_night.jpg"
+fi
